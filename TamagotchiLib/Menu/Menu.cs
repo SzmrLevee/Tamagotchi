9eb200f3cc07902dbf1b64d466a370cc911f7acf
Levente Szomor <59541573+SzmrLevee@users.noreply.github.com>
2025-01-04 18:48:52 +0100

    Menü logikájának elkészítése

---
+++ TamagotchiLib/Menu/Menu.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace TamagotchiLib.Menu
+{
+    public class Menu
+    {
+        private int SelectedOption;
+        private string[] Options;
+        private string Prompt;
+
+        public Menu(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            Options = options;
+            SelectedOption = 0;
+        }
+
+        public void DisplayOptions()
+        {
+            WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+                string prefix;
+
+                if (i == SelectedOption)
+                {
+                    prefix = "*";
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    prefix = " ";
+                    ForegroundColor = ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+
+                WriteLine($"{prefix} << {currentOption} >>");
+            }
+            ResetColor();
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Clear();
+                DisplayOptions();
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                // Update the selected index with arrow keys.
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedOption--;
+                    if (SelectedOption == -1)
+                    {
+                        SelectedOption = Options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedOption++;
+                    if (SelectedOption == Options.Length)
+                    {
+                        SelectedOption = 0;
+                    }
+                }
+
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedOption;
+        }
+    }
+}
