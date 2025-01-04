dee3a5abed97947b004c0a6f6c76887cfc053188
Levente Szomor <59541573+SzmrLevee@users.noreply.github.com>
2025-01-04 18:53:50 +0100

    Játékok kidolgozása

--- TamagotchiLib/MiniGames/ColorGuessGame.cs
+++ TamagotchiLib/MiniGames/ColorGuessGame.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TamagotchiLib.Models;
 
 namespace TamagotchiLib.MiniGames
 {
-    internal class ColorGuessGame
+    public class ColorGuessGame
     {
+        private readonly Random random = new Random();
+
+        public void Play(Pet pet)
+        {
+            Console.WriteLine("Találd ki, hogy piros vagy kék lesz a szín! Írd be: 'piros' vagy 'kék'.");
+            string userChoice = Console.ReadLine()?.ToLower();
+
+            string[] colors = { "piros", "kék" };
+            string randomColor = colors[random.Next(colors.Length)];
+
+            Console.WriteLine($"A választott szín: {randomColor}");
+
+            if (userChoice == randomColor)
+            {
+                Console.WriteLine("Gratulálok! Eltaláltad.");
+                pet.Mood = Math.Min(100, pet.Mood + 10);
+                pet.Money += 20;
+            }
+            else
+            {
+                Console.WriteLine("Nem találtad el. Próbáld újra!");
+                pet.Mood = Math.Min(100, pet.Mood + 5);
+            }
+        }
     }
 }
