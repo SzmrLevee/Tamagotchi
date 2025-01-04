f904bf72db3a4acfb1bd3f4a59555bb6512e3501
Levente Szomor <59541573+SzmrLevee@users.noreply.github.com>
2025-01-04 18:58:12 +0100

    Adott játék kiválasztásának kódja

--- TamagotchiLib/GameManager.cs
+++ TamagotchiLib/GameManager.cs
@@ -1,7 +1,49 @@
-﻿namespace TamagotchiLib
+﻿using TamagotchiLib.MiniGames;
+using TamagotchiLib.Models;
+using TamagotchiLib.Utils;
+
+namespace TamagotchiLib
 {
     public class GameManager
     {
+        public Pet CurrentPet { get; private set; }
+        private readonly FileManager fileManager = new FileManager("Data/pet_data.json");
 
+        public GameManager()
+        {
+            LoadPet();
+        }
+
+        public void LoadPet()
+        {
+            CurrentPet = fileManager.LoadData<Pet>() ?? new Pet("Kisállatka");
+        }
+
+        public void SavePet()
+        {
+            fileManager.SaveData(CurrentPet);
+        }
+
+        public void PlayMinigame(int gameChoice)
+        {
+            switch (gameChoice)
+            {
+                case 1:
+                    new ColorGuessGame().Play(CurrentPet);
+                    break;
+                case 2:
+                    new MathChallengeGame().Play(CurrentPet);
+                    break;
+                default:
+                    Console.WriteLine("Érvénytelen játékválasztás.");
+                    break;
+            }
+            SavePet();
+        }
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine(CurrentPet.ToString());
+        }
     }
 }
