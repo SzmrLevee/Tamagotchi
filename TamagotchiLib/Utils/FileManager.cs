287e727c1e4e09b1166922e087365a45f6f0c977
Levente Szomor <59541573+SzmrLevee@users.noreply.github.com>
2025-01-04 18:56:42 +0100

    Fileok tárolásának és betöltésének elkezdése

--- TamagotchiLib/Utils/FileManager.cs
+++ TamagotchiLib/Utils/FileManager.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace TamagotchiLib.Utils
 {
-    internal class FileManager
+    public class FileManager
     {
+        private readonly string filePath;
+
+        public FileManager(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public T LoadData<T>() where T : class
+        {
+            if (!File.Exists(filePath)) return null;
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(json);
+        }
+
+        public void SaveData<T>(T data)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(data, options);
+            File.WriteAllText(filePath, json);
+        }
     }
 }
